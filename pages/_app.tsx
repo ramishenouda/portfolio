40c662efc1230a6952621a27279eed98f3780c8a
ramishenouda <ramishenouda@outlook.com>
2022-11-28 22:20:15 +0200

    created a new project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,6 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { Sidebar } from '../components/Sidebar/Sidebar';
-import { Footer } from '../components/Footer/Footer';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div className="container flex">
-      <Sidebar />
-      <Component {...pageProps} />
-    </div>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp;
