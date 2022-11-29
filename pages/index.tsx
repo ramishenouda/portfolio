86bb8d9786a5cc9f8c8686e3ea5400e3e166b0e9
ramishenouda <ramishenouda@outlook.com>
2022-11-30 00:21:58 +0200

    added basic navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Intro from '../components/intro';
+import Navbar from '../components/navbar';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
 
       <main>
+        <Navbar />
         <Intro />
       </main>
     </div>
