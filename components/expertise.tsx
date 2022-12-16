371d6e5914ed9b6a29468271264ad50b297032b6
ramishenouda <ramishenouda@outlook.com>
2022-12-16 22:47:05 +0200

    finished the expertise section

--- components/expertise.tsx
+++ components/expertise.tsx
@@ -2,6 +2,20 @@ import Image, { StaticImageData } from 'next/image';
 import softwareIcon from '../public/computer.png';
 import reactIcon from '../public/react.png';
 import backendIcon from '../public/backend.png';
+import javascriptIcon from '../public/javascript.png';
+import typescriptIcon from '../public/typescript.png';
+import htmlIcon from '../public/html.png';
+import cssIcon from '../public/css.png';
+import bootstrapIcon from '../public/bootstrap.png';
+import tailwindIcon from '../public/tailwind.png';
+import angularIcon from '../public/angular.png';
+import nextjsIcon from '../public/nextjs.png';
+import csharpIcon from '../public/csharp.png';
+import gitIcon from '../public/git.png';
+import dockerIcon from '../public/docker.png';
+import problemsolverIcon from '../public/problemsolver.png';
+import designIcon from '../public/design.png';
+import stars from '../public/stars.png';
 
 export default function Expertise() {
   const softwareDev = {
@@ -29,13 +43,16 @@ export default function Expertise() {
   return (
     <section id="expertise" className="min-h-[100vh] text-white gap-3 flex flex-col items-center justify-center">
       <div className="text-center rounded-3xl">
-        <h1 className="md:text-[5vw] uppercase tracking-widest mb-4">My Expertise</h1>
-        <hr className="hr-text mt-1" data-content={'<CoffeScript ☕  /> '} />
-        <p className="text-xl tracking-widest font-medium mt-8">
-          {"I'm"} always seeking new opportunities to learn, especially the opportunities that can push your projects
-          towards the outer galaxy..
-        </p>
-        <div className="flex md:flex-row flex-col align-middle justify-center gap-4 mt-8">
+        <h1 className="md:text-[5vw] uppercase tracking-widest mb-5 mt-5">My Expertise</h1>
+        <hr className="hr-text mt-2" data-content={'<CoffeScript ☕  /> '} />
+        <div className="expertise-intro lg:flex flex-row justify-center items-center">
+          <Image className="mx-auto" src={stars} alt="stars" />
+          <p className="text-4xl ml-4 p-4 tracking-widest font-medium mt-8">
+            {"I'm"} always seeking new opportunities to learn, especially the opportunities that can push your projects
+            towards the outer galaxy..
+          </p>
+        </div>
+        <div className="flex md:flex-row flex-col align-middle justify-center gap-2 flex-1 w-full mt-8">
           {expert(softwareIcon, softwareDev.title, softwareDev.description, softwareDev.experienced)}
           {expert(reactIcon, frontendDev.title, frontendDev.description, frontendDev.experienced)}
           {expert(backendIcon, backendDev.title, backendDev.description, backendDev.experienced)}
@@ -43,7 +60,36 @@ export default function Expertise() {
       </div>
       <div className="text-center">
         <h1 className="md:text-[2vw] uppercase tracking-widest ">My favourite tech</h1>
-        <hr className="hr-text mt-1" data-content={'<CoffeScript ☕  /> '} />
+        <hr className="hr-text mt-2" data-content={'<CoffeScript ☕  /> '} />
+        <div className="grid grid-cols-3 w-[90vw] lg:w-[75vw] gap-4 mt-5">
+          <div>{tech(javascriptIcon, 'JavaScript')}</div>
+          <div>{tech(typescriptIcon, 'TypeScript')}</div>
+          <div>{tech(null, 'Nest.js')}</div>
+        </div>
+        <div className="grid grid-cols-2 lg:grid-cols-4 w-[90vw] lg:w-[75vw] gap-4 mt-5">
+          <div>{tech(htmlIcon, 'HTML')}</div>
+          <div>{tech(cssIcon, 'CSS')}</div>
+          <div>{tech(bootstrapIcon, 'Bootstrap')}</div>
+          <div>{tech(tailwindIcon, 'Tailwind CSS')}</div>
+        </div>
+        <div className="grid grid-cols-3 w-[90vw] lg:w-[75vw] gap-4 mt-5">
+          <div>{tech(angularIcon, 'Angular')}</div>
+          <div>{tech(reactIcon, 'React')}</div>
+          <div>{tech(nextjsIcon, '')}</div>
+        </div>
+        <div className="grid grid-cols-3 w-[90vw] lg:w-[75vw] gap-4 mt-5">
+          <div>{tech(csharpIcon, '')}</div>
+          <div>{tech(null, 'ASP.NET Core')}</div>
+          <div>{tech(null, 'Entity Framework')}</div>
+        </div>
+        <div className="grid grid-cols-3 w-[90vw] lg:w-[75vw] gap-4 mt-5">
+          <div>{tech(gitIcon, 'Git')}</div>
+          <div>{tech(dockerIcon, 'Docker')}</div>
+          <div>{tech(problemsolverIcon, 'Problem Solver')}</div>
+        </div>
+        <div className="grid grid-cols-1 w-[90vw] lg:w-[75vw] gap-4 mt-5">
+          <div>{tech(designIcon, 'Transform PSD / Wireframes into web applications')}</div>
+        </div>
       </div>
     </section>
   );
@@ -51,9 +97,9 @@ export default function Expertise() {
 
 const expert = (icon: StaticImageData, title: string, description: string, experienced?: string) => {
   return (
-    <div className="border-white border-2 max-w-[500px] px-8 py-4">
+    <div className="border-white border-2 flex-1 px-8 py-4">
       <div className="flex flex-grow flex-row items-center">
-        <Image src={icon} alt="software dev icon" />
+        <Image src={icon} alt="expert icon" />
         <h1 className="ml-2 text-3xl w-full"> {title} </h1>
       </div>
       <div className="flex flex-row items-center justify-center mt-4">
@@ -68,6 +114,15 @@ const expert = (icon: StaticImageData, title: string, description: string, exper
           <p className="text-xl mt-2">{experienced && experienced}</p>
         </div>
       </div>
+    </div>
+  );
+};
+
+const tech = (icon: StaticImageData | null, title: string) => {
+  return (
+    <div className="flex min-h-full flex-row border-2 w-full p-2 justify-center items-center">
+      <p className="text-2xl font-bold uppercase">{title}</p>
+      {icon && <Image className="ml-4" src={icon} alt="tech icon" />}
     </div>
   );
 };
