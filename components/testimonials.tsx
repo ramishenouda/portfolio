126dc7cbd853a38e22768a7b9e46e1f9d79dcc3f
ramishenouda <ramishenouda@outlook.com>
2023-01-01 09:40:31 +0200

    feat: added testimonials section

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,12 +1,83 @@
+import Link from 'next/link';
+
 export default function Testimonials() {
+  const testimoinals = [
+    {
+      description:
+        'Thank you Rami for your good work! Rami does his best to complete the projects successfully and on time, even if the requirements are not well explained! Will hire Rami for my next projects again!',
+      link: 'https://codekiste.com/',
+      linkTitle: '~Arek Roczniewski - Roczniewski & Schwede GbR',
+    },
+    {
+      description:
+        'Rami have shown commitment, well-organized work, and support to his colleagues. Rami is a great asset to our team, he is encouraged to speak up to share his thoughts, to involve in the planned tasks.',
+      link: 'https://www.linkedin.com/in/ihamdeen/',
+      linkTitle: '~Mohamed Hamdeen - founder of DrugCatcher',
+    },
+    {
+      description:
+        'Rami helped me with everything, including explaining the differences between my options and he helped me publish my website. He also helped me choose a good server provider. He is really experienced and knows what he is doing. He is respectful and qualified.',
+      link: 'https://www.deryadinc.com/',
+      linkTitle: '~Derya Dinc',
+    },
+  ];
+
   return (
-    <div id="testimonials" className="intro min-h-[100vh] text-white pt-28  flex flex-col items-center">
-      <div className="intro-text text-center rounded-3xl">
-        <h1 className="text-[5vw] tracking-widest">Testimonials</h1>
-        <p className="text-2xl tracking-widest font-medium">
-          A programmer who is constantly seeking new learning opportunities.
-        </p>
+    <div id="testimonials" className="intro min-h-[100vh] text-white    flex flex-col items-center justify-center">
+      <div className="text-center rounded-3xl">
+        <h1 className="text-[5vw] !uppercase tracking-widest my-5">
+          <span className="text-2xl">04.</span>Testimonials
+        </h1>
+        <hr className="mt-2 hr-text" data-content={'<My babies 👶 /> '} />
+      </div>
+
+      <p className="mt-16 text-4xl">Feedback from clients on great experiences</p>
+      <div className="">
+        <div className="flex flex-col gap-8">
+          <div className=" max-w-[95vw] items-center overflow-x-hidden">
+            <div id="testimonials-container" className="flex flex-row transition duration-1000">
+              {testimonial(testimoinals[0].description, testimoinals[0].linkTitle, testimoinals[0].link)}
+              {testimonial(testimoinals[1].description, testimoinals[1].linkTitle, testimoinals[1].link)}
+              {testimonial(testimoinals[2].description, testimoinals[2].linkTitle, testimoinals[2].link)}
+            </div>
+          </div>
+          <div className="flex flex-row items-center justify-center gap-8">
+            <p onClick={() => getTestimonial(0)} className="cursor-pointer text-9xl">
+              .
+            </p>
+            <p onClick={() => getTestimonial(1)} className="cursor-pointer text-9xl">
+              .
+            </p>
+            <p onClick={() => getTestimonial(2)} className="cursor-pointer text-9xl">
+              .
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
+const testimonial = (description: string, linkTitle: string, link: string) => {
+  return (
+    <div className="min-w-[95vw] mt-12">
+      <p className="text-2xl tracking-widest text-center max-w-8xl">{description}</p>
+      <p className="mt-12 text-center">
+        <Link className="text-xl text-teal-400" href={link}>
+          {linkTitle}
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+const getTestimonial = (id: number) => {
+  const testimonialsContainer = document.getElementById('testimonials-container');
+  const position = id * -95;
+
+  if (!testimonialsContainer) {
+    return;
+  }
+
+  testimonialsContainer.style.transform = `translateX(${position}vw)`;
+};
