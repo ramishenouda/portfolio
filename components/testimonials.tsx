c1ab55cf06539d015121e08949eadbd53b7ceb05
ramishenouda <ramishenouda@outlook.com>
2023-01-01 16:22:33 +0200

    added current testimonial indicator

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,26 +1,29 @@
 import Link from 'next/link';
+import React from 'react';
+
+const testimoinals = [
+  {
+    description:
+      'Thank you Rami for your good work! Rami does his best to complete the projects successfully and on time, even if the requirements are not well explained! Will hire Rami for my next projects again!',
+    link: 'https://codekiste.com/',
+    linkTitle: '~Arek Roczniewski - Roczniewski & Schwede GbR',
+  },
+  {
+    description:
+      'Rami have shown commitment, well-organized work, and support to his colleagues. Rami is a great asset to our team, he is encouraged to speak up to share his thoughts, to involve in the planned tasks.',
+    link: 'https://www.linkedin.com/in/ihamdeen/',
+    linkTitle: '~Mohamed Hamdeen - founder of DrugCatcher',
+  },
+  {
+    description:
+      'Rami helped me with everything, including explaining the differences between my options and he helped me publish my website. He also helped me choose a good server provider. He is really experienced and knows what he is doing. He is respectful and qualified.',
+    link: 'https://www.deryadinc.com/',
+    linkTitle: '~Derya Dinc',
+  },
+];
 
 export default function Testimonials() {
-  const testimoinals = [
-    {
-      description:
-        'Thank you Rami for your good work! Rami does his best to complete the projects successfully and on time, even if the requirements are not well explained! Will hire Rami for my next projects again!',
-      link: 'https://codekiste.com/',
-      linkTitle: '~Arek Roczniewski - Roczniewski & Schwede GbR',
-    },
-    {
-      description:
-        'Rami have shown commitment, well-organized work, and support to his colleagues. Rami is a great asset to our team, he is encouraged to speak up to share his thoughts, to involve in the planned tasks.',
-      link: 'https://www.linkedin.com/in/ihamdeen/',
-      linkTitle: '~Mohamed Hamdeen - founder of DrugCatcher',
-    },
-    {
-      description:
-        'Rami helped me with everything, including explaining the differences between my options and he helped me publish my website. He also helped me choose a good server provider. He is really experienced and knows what he is doing. He is respectful and qualified.',
-      link: 'https://www.deryadinc.com/',
-      linkTitle: '~Derya Dinc',
-    },
-  ];
+  let [currentTestimonial, setCurrentTestimonial] = React.useState(0);
 
   return (
     <div id="testimonials" className="intro min-h-[100vh] text-white    flex flex-col items-center justify-center">
@@ -42,15 +45,78 @@ export default function Testimonials() {
             </div>
           </div>
           <div className="flex flex-row items-center justify-center gap-8">
-            <p onClick={() => getTestimonial(0)} className="cursor-pointer text-9xl">
-              .
-            </p>
-            <p onClick={() => getTestimonial(1)} className="cursor-pointer text-9xl">
-              .
-            </p>
-            <p onClick={() => getTestimonial(2)} className="cursor-pointer text-9xl">
-              .
-            </p>
+            <div
+              onClick={() => {
+                getTestimonial(0);
+                setCurrentTestimonial(0);
+              }}
+            >
+              <p
+                className={`${
+                  currentTestimonial === 0 && 'text-black'
+                } transition duration-1000 cursor-pointer text-9xl `}
+              >
+                <span
+                  className={`${currentTestimonial !== 0 ? 'text-background' : 'text-white '} transition duration-1000`}
+                >
+                  {'<'}
+                </span>
+                .
+                <span
+                  className={`${currentTestimonial !== 0 ? 'text-background' : 'text-white '} transition duration-1000`}
+                >
+                  {'/>'}
+                </span>
+              </p>
+            </div>
+            <div
+              onClick={() => {
+                getTestimonial(1);
+                setCurrentTestimonial(1);
+              }}
+            >
+              <p
+                className={`${
+                  currentTestimonial === 1 && 'text-black'
+                } transition duration-1000 cursor-pointer text-9xl `}
+              >
+                <span
+                  className={`${currentTestimonial !== 1 ? 'text-background' : 'text-white '} transition duration-1000`}
+                >
+                  {'<'}
+                </span>
+                .
+                <span
+                  className={`${currentTestimonial !== 1 ? 'text-background' : 'text-white '} transition duration-1000`}
+                >
+                  {'/>'}
+                </span>
+              </p>
+            </div>
+            <div
+              onClick={() => {
+                getTestimonial(2);
+                setCurrentTestimonial(2);
+              }}
+            >
+              <p
+                className={`${
+                  currentTestimonial === 2 && 'text-black'
+                } transition duration-1000 cursor-pointer text-9xl `}
+              >
+                <span
+                  className={`${currentTestimonial !== 2 ? 'text-background' : 'text-white '} transition duration-1000`}
+                >
+                  {'<'}
+                </span>
+                .
+                <span
+                  className={`${currentTestimonial !== 2 ? 'text-background' : 'text-white '} transition duration-1000`}
+                >
+                  {'/>'}
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
