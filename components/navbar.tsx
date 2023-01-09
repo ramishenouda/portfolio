85b6888e1f611b9761a1972207933ab4e367c5d1
ramishenouda <ramishenouda@outlook.com>
2023-01-09 08:12:41 +0200

    feat: added some touch for expertise

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div>
-      <div className="navbar top-8 z-40 fixed gap-5 flex flex-row w-full text-white items-center justify-center">
+      <div className="navbar top-0 py-4 z-40 fixed gap-5 flex flex-row w-full bg-background h-[50px] text-white items-center justify-center">
         <div className="nav-logo z-50 fixed left-8 text-left">
           <span className="text-4xl ">aRami</span>.me
         </div>
