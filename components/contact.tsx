dd8d0ebb2b64cb73b91916639520e99918b087cf
ramishenouda <ramishenouda@outlook.com>
2023-01-19 10:06:57 +0200

    contect style update

--- components/contact.tsx
+++ components/contact.tsx
@@ -11,20 +11,20 @@ export default function Contact() {
   };
 
   return (
-    <div id="contact" className="intro my-auto flex flex-col justify-center min-h-[100vh] text-white">
-      <div className="intro-text rounded-3xl">
-        <h1 className="md:text-8xl text-4xl tracking-widest uppercase">
-          <span className="text-lg md:text-2xl">05.</span>Contact
-        </h1>
+    <div id="contact" className="flex flex-col mb-4 justify-center min-h-[100vh]">
+      <div className="rounded-3xl">
+        <div className="md:text-8xl font-bold text-4xl tracking-widest uppercase">
+          <span className="text-lg md:text-2xl">05.</span>HANDSHAKE?
+        </div>
       </div>
-      <div className="flex w-full flex-col justify-center items-center md:flex-row gap-12 px-12 mt-12">
-        <form className="flex flex-col  flex-1 rounded shadow-sm">
+      <div className="flex w-full flex-col justify-center items-center lg:flex-row gap-12 md:px-12 mt-12">
+        <form className="flex flex-col w-full flex-1 rounded shadow-sm">
           <Input size="xl" clearable bordered labelPlaceholder="Name" />
-          <Spacer y={2.5} />
+          <Spacer y={2} />
           <Input shadow className="w-full" labelPlaceholder="Email" bordered type={'email'} size="xl" />
-          <Spacer y={2.5} />
+          <Spacer y={2} />
           <Textarea size="xl" labelPlaceholder="Message" minRows={5} maxRows={10} bordered />
-          <Spacer y={2.5} />
+          <Spacer y={2} />
           <Button
             bordered
             color="secondary"
@@ -39,21 +39,21 @@ export default function Contact() {
           </Button>
         </form>
         <div className="flex flex-col justify-center items-center">
-          <div>
-            <h1>Contact details</h1>
+          <div className="text-center">
+            <p className="text-5xl md:text-7xl">Contact details</p>
             <div className="child:text-2xl mt-8 flex flex-col gap-4">
               <div>
-                <Link className="flex flex-row gap-2" href="mailto:ramishenouda@outlook.com">
+                <Link className="child:text-center flex flex-row gap-2" href="mailto:ramishenouda@outlook.com">
                   <AiOutlineMail size="35px" /> ramishenouda@outlook.com
                 </Link>
               </div>
-              <div className="flex flex-row gap-2">
+              <div className="child:text-center lg:justify-start justify-center items-center flex flex-row gap-2">
                 <AiOutlinePhone size="35px" /> +201551874208
               </div>
             </div>
           </div>
-          <div className="mt-24 mb-4 md:mb-0">
-            <h1>Social media</h1>
+          <div className="lg:mt-24 lg:mb-0 my-8 md:mb-0">
+            <p className="text-5xl md:text-7xl">Social media</p>
             <div className="flex flex-row gap-8 child:text-2xl mt-8 justify-center items-center">
               <Link target="_blank" href="https://github.com/ramishenouda/">
                 <FiGithub size="35px" />
