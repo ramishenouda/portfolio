3697476170bc6ac3264e55e653a539d6017a1832
ramishenouda <ramishenouda@outlook.com>
2023-01-19 09:24:02 +0200

    testimonials style update

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -26,95 +26,96 @@ export default function Testimonials() {
   let [currentTestimonial, setCurrentTestimonial] = React.useState(0);
 
   return (
-    <div id="testimonials" className="min-h-[100vh] flex flex-col items-center justify-center">
-      <div className="flex flex-row items-end w-full text-left">
-        <div className="text-lg md:text-2xl text-neutral-200 pb-1 md:pb-2">04.</div>
-        <div className="text-4xl md:text-8xl !uppercase">Testimonials</div>
+    <div id="testimonials" className="min-h-[80vh] justify-center flex flex-col">
+      <div className="rounded-3xl">
+        <h1 className="md:text-8xl text-4xl tracking-widest uppercase">
+          <span className="text-lg md:text-2xl">04.</span>TESTIMONIALS
+        </h1>
       </div>
-      <p className="mt-16 text-4xl">Feedback from clients on great experiences</p>
-      <div className="">
-        <div className="flex flex-col gap-8">
-          <div className="w-[95vw] items-center overflow-x-hidden">
-            <div id="testimonials-container" className="flex flex-row transition duration-1000">
-              {testimonial(testimoinals[0].description, testimoinals[0].linkTitle, testimoinals[0].link)}
-              {testimonial(testimoinals[1].description, testimoinals[1].linkTitle, testimoinals[1].link)}
-              {testimonial(testimoinals[2].description, testimoinals[2].linkTitle, testimoinals[2].link)}
+      <p className="md:mt-16 tracking-widest text-center mt-8 text-2xl font-medium md:text-4xl">
+        Feedback from clients on great experiences
+      </p>
+      <div className="flex flex-col items-center justify-center gap-8">
+        <div className="w-[90vw] md:w-[85vw] overflow-x-hidden mt-4 md:mt-12">
+          <div id="testimonials-container" className="flex flex-row transition duration-1000">
+            {testimonial(testimoinals[0].description, testimoinals[0].linkTitle, testimoinals[0].link)}
+            {testimonial(testimoinals[1].description, testimoinals[1].linkTitle, testimoinals[1].link)}
+            {testimonial(testimoinals[2].description, testimoinals[2].linkTitle, testimoinals[2].link)}
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-center gap-8">
+          <div
+            onClick={() => {
+              getTestimonial(0);
+              setCurrentTestimonial(0);
+            }}
+          >
+            <div
+              className={`${
+                currentTestimonial !== 0 && 'text-background'
+              } transition duration-1000 cursor-pointer text-2xl `}
+            >
+              {'<'}
+              <span className={`${currentTestimonial !== 0 ? 'opacity-50' : 'opacity-100'} transition duration-1000`}>
+                🙂
+              </span>
+              <span
+                className={`${currentTestimonial !== 0 ? 'text-background' : 'text-white '} transition duration-1000`}
+              >
+                {'/>'}
+              </span>
             </div>
           </div>
-          <div className="flex flex-row items-center justify-center gap-8">
+          <div
+            onClick={() => {
+              getTestimonial(1);
+              setCurrentTestimonial(1);
+            }}
+          >
             <div
-              onClick={() => {
-                getTestimonial(0);
-                setCurrentTestimonial(0);
-              }}
+              className={`${
+                currentTestimonial === 1 && 'text-black'
+              } transition duration-1000 cursor-pointer text-2xl `}
             >
-              <div
-                className={`${
-                  currentTestimonial !== 0 && 'text-background'
-                } transition duration-1000 cursor-pointer text-2xl `}
+              <span
+                className={`${currentTestimonial !== 1 ? 'text-background' : 'text-white '} transition duration-1000`}
               >
                 {'<'}
-                <span className={`${currentTestimonial !== 0 ? 'opacity-50' : 'opacity-100'} transition duration-1000`}>
-                  🙂
-                </span>
-                <span
-                  className={`${currentTestimonial !== 0 ? 'text-background' : 'text-white '} transition duration-1000`}
-                >
-                  {'/>'}
-                </span>
-              </div>
-            </div>
-            <div
-              onClick={() => {
-                getTestimonial(1);
-                setCurrentTestimonial(1);
-              }}
-            >
-              <div
-                className={`${
-                  currentTestimonial === 1 && 'text-black'
-                } transition duration-1000 cursor-pointer text-2xl `}
+              </span>
+              <span className={`${currentTestimonial !== 1 ? 'opacity-50' : 'opacity-100'} transition duration-1000`}>
+                🙂
+              </span>
+              <span
+                className={`${currentTestimonial !== 1 ? 'text-background' : 'text-white '} transition duration-1000`}
               >
-                <span
-                  className={`${currentTestimonial !== 1 ? 'text-background' : 'text-white '} transition duration-1000`}
-                >
-                  {'<'}
-                </span>
-                <span className={`${currentTestimonial !== 1 ? 'opacity-50' : 'opacity-100'} transition duration-1000`}>
-                  🙂
-                </span>
-                <span
-                  className={`${currentTestimonial !== 1 ? 'text-background' : 'text-white '} transition duration-1000`}
-                >
-                  {'/>'}
-                </span>
-              </div>
+                {'/>'}
+              </span>
             </div>
+          </div>
+          <div
+            onClick={() => {
+              getTestimonial(2);
+              setCurrentTestimonial(2);
+            }}
+          >
             <div
-              onClick={() => {
-                getTestimonial(2);
-                setCurrentTestimonial(2);
-              }}
+              className={`${
+                currentTestimonial === 2 && 'text-black'
+              } transition duration-1000 cursor-pointer text-2xl `}
             >
-              <div
-                className={`${
-                  currentTestimonial === 2 && 'text-black'
-                } transition duration-1000 cursor-pointer text-2xl `}
+              <span
+                className={`${currentTestimonial !== 2 ? 'text-background' : 'text-white '} transition duration-1000`}
               >
-                <span
-                  className={`${currentTestimonial !== 2 ? 'text-background' : 'text-white '} transition duration-1000`}
-                >
-                  {'<'}
-                </span>
-                <span className={`${currentTestimonial !== 2 ? 'opacity-50' : 'opacity-100'} transition duration-1000`}>
-                  🙂
-                </span>
-                <span
-                  className={`${currentTestimonial !== 2 ? 'text-background' : 'text-white '} transition duration-1000`}
-                >
-                  {'/>'}
-                </span>
-              </div>
+                {'<'}
+              </span>
+              <span className={`${currentTestimonial !== 2 ? 'opacity-50' : 'opacity-100'} transition duration-1000`}>
+                🙂
+              </span>
+              <span
+                className={`${currentTestimonial !== 2 ? 'text-background' : 'text-white '} transition duration-1000`}
+              >
+                {'/>'}
+              </span>
             </div>
           </div>
         </div>
@@ -125,8 +126,8 @@ export default function Testimonials() {
 
 const testimonial = (description: string, linkTitle: string, link: string) => {
   return (
-    <div className="min-w-[95vw] mt-12">
-      <p className="text-2xl tracking-widest text-center max-w-8xl">{description}</p>
+    <div className="min-w-[90vw] md:min-w-[85vw] text-center">
+      <p className="md:text-2xl text-xl w-full">{description}</p>
       <div className="mt-12 text-center">
         <Link className="text-xl text-teal-400" href={link}>
           {linkTitle}
