d72ce3781aa127d338183a4ee00df69164aa57b8
ramishenouda <ramishenouda@outlook.com>
2023-02-02 16:15:19 +0200

    experience component refactor

--- components/experience/experience.tsx
+++ components/experience/experience.tsx
@@ -1,6 +1,5 @@
-import Link from 'next/link';
-import { MdLocationOn } from 'react-icons/md';
 import { useState } from 'react';
+import { ExperienceItem } from './experience-item';
 
 export default function Experience() {
   const [company, setCompany] = useState(0);
@@ -35,99 +34,37 @@ export default function Experience() {
       </div>
       <div className="md:ml-10 mt-5 flex  p-1 justify-center items-center flex-col lg:flex-row md:gap-20">
         <div className="flex-1 max-w-[1000px]">
-          {experience(
-            'Do IT Big!',
-            'Full-stack developer (Contract)',
-            '2021 - Present',
-            'Netherlands',
-            doItBigPoints,
-            company === 0,
-            () => setCompany(company === 0 ? -1 : 0),
-            'doitbig.nl',
-          )}
-          {experience(
-            'Roczniewski & Schwede GbR ',
-            'Game Developer (Contract)',
-            '2020 - 2021',
-            'Germany',
-            RoczniewskiSchwedeGbRPoints,
-            company === 1,
-            () => setCompany(company === 1 ? -1 : 1),
-            'codekiste.com',
-          )}
-          {experience(
-            'DrugCatcher',
-            'Full-stack developer (Internship)',
-            'Jul 2020 - Nov 2020',
-            'Alexandria, Egypt',
-            drugCatcherPoints,
-            company === 2,
-            () => setCompany(company === 2 ? -1 : 2),
-          )}
+          <ExperienceItem
+            companyName="Do IT Big!"
+            jobTitle="Full-stack developer (Contract)"
+            date="2021 - Present"
+            location="Netherlands"
+            bulletPoints={doItBigPoints}
+            currentCompany={company === 0}
+            setCompany={() => setCompany(company === 0 ? -1 : 0)}
+            companyLink="doitbig.nl"
+          />
+          <ExperienceItem
+            companyName="Roczniewski & Schwede GbR"
+            jobTitle="Game Developer (Contract)"
+            date="2020 - 2021"
+            location="Germany"
+            bulletPoints={RoczniewskiSchwedeGbRPoints}
+            currentCompany={company === 1}
+            setCompany={() => setCompany(company === 1 ? -1 : 1)}
+            companyLink="codekiste.com"
+          />
+          <ExperienceItem
+            companyName="DrugCatcher"
+            jobTitle="Full-stack developer (Internship)"
+            date="Jul 2020 - Nov 2020"
+            location="Alexandria, Egypt"
+            bulletPoints={drugCatcherPoints}
+            currentCompany={company === 2}
+            setCompany={() => setCompany(company === 2 ? -1 : 2)}
+          />
         </div>
       </div>
     </div>
   );
 }
-
-const experience = (
-  companyName: string,
-  jobTitle: string,
-  date: string,
-  location: string,
-  bulletPoints: Array<string>,
-  currentCompany: boolean,
-  setCompany: () => void,
-  companyLink?: string,
-) => {
-  return (
-    <div className={`w-full company-experience mb-4 `}>
-      <div
-        onClick={() => setCompany()}
-        className={`bg-info transition-all duration-500 cursor-pointer  p-4 rounded-md font-semibold flex flex-col md:flex-row ${
-          currentCompany && `!bg-info-50`
-        }`}
-      >
-        <div className="flex-1 md:block flex flex-row justify-between child:text-md md:child:text-2xl">
-          <div>
-            <span>{companyName}</span>
-            <span className="md:inline hidden"> - {jobTitle}</span>
-            <div className="md:hidden block">{jobTitle}</div>
-          </div>
-          <div className="md:hidden block">{currentCompany ? '-' : '+'}</div>
-        </div>
-        <div className="mr-2 child:text-md md:child:text-2xl">
-          <span className="mr-10">{date}</span>
-          <span className="md:inline hidden">{currentCompany ? '-' : '+'}</span>
-        </div>
-      </div>
-      <div
-        className={`rounded-md max-h-0 bg-blue-900/20  overflow-y-hidden transition-all duration-500 ${
-          currentCompany && `p-2  px-8 mt-4 max-h-[1000px]`
-        }`}
-      >
-        <div className="mt-2 child:text-lg md:child:text-xl flex flex-row gap-8">
-          <div className="flex flex-row gap-2 items-center justify-center">
-            <MdLocationOn color="2e5cda" size={20} /> {location}
-          </div>
-          {companyLink && (
-            <div className="hover:text-purple-400">
-              <Link className="text-blue-400 hover:text-blue-200" target="_blank" href={`https://` + companyLink}>
-                <span className="text-xl mr-2">↗</span> {companyLink}
-              </Link>
-            </div>
-          )}
-        </div>
-        <ul className=" mt-4">
-          {bulletPoints.map((point, index) => {
-            return (
-              <p key={point + index} className="text-lg md:text-xl mb-2">
-                - {point}
-              </p>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
-};
