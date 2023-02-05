518eb2dc98ec366e2a79fabae79ce200ef20c014
ramishenouda <ramishenouda@outlook.com>
2023-02-05 22:29:30 +0200

    added project animations

--- components/projects/project-item.tsx
+++ components/projects/project-item.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { IconType } from 'react-icons';
 
 import { BsArrowUpRight } from 'react-icons/bs';
 import { FaStripe } from 'react-icons/fa';
@@ -11,6 +12,7 @@ type props = {
   descriptionInfo: string;
   projectLink: string;
   imagePath: string;
+  techIcons: Array<IconType>;
   reverseOrder?: boolean;
 };
 
@@ -20,46 +22,46 @@ export const ProjectItem = ({
   descriptionInfo,
   projectLink,
   imagePath,
+  techIcons,
   reverseOrder,
 }: props) => {
   return (
     <div className="min-h-[85vh] md:min-h-screen">
-      <Link
-        target={'_blank'}
-        href={projectLink}
-        className={`hover:text-blue-300 ${reverseOrder ? '' : 'md:text-right'} transition-colors duration-500`}
-      >
-        <h1 className="md:bg-transparent mb-5 md:text-6xl text-4xl">{title}</h1>
-      </Link>
+      <div className={` ${reverseOrder ? '' : 'flex justify-end'}`}>
+        <Link
+          target={'_blank'}
+          href={projectLink}
+          className={`hover:text-blue-300 ${
+            reverseOrder ? '' : 'md:text-right'
+          } text-right ml-auto transition-colors duration-500`}
+        >
+          <h1 className="md:bg-transparent w-max mb-5 inline-block md:text-5xl text-4xl">{title}</h1>
+        </Link>
+      </div>
       <div className={`flex ${reverseOrder ? 'flex-row-reverse' : 'flex-row'} relative`}>
-        <div className="md:w-3/4 absolute md:sticky top-3/4">
+        <div className="hover:scale-[1.02]  transition-all duration-500 md:w-3/4 absolute md:sticky top-3/4">
           <Image src={imagePath} height={500} width={500} className="h-full w-full" alt="foodeli" />
         </div>
         <div className="md:w-1/4 md:z-1 z-50 w-full">
           <div
-            className={`absolute left-0 md:left-[unset] top-0 right-0 max-w-[400px] z-50 justify-center items-center  ${
+            className={`absolute left-0 md:left-[unset] top-0 right-0 max-w-[500px] z-50 justify-center items-center  ${
               reverseOrder ? '!left-0' : ''
             }`}
           >
-            <div className=" bg-black/70 p-8 flex flex-col">
+            <div className="hover:scale-105 transition-all duration-500 bg-black/70 p-8 flex flex-col">
               <p className="text-2xl md:text-3xl">
                 {descriptionHeader}
                 <br />
                 {descriptionInfo}
               </p>
             </div>
-            <div className={`bg-black/70 mt-3 p-3 text-xl font-bold rounded-sm`}>
+            <div
+              className={`hover:scale-105 transition-all duration-500 bg-black/70 mt-3 p-3 text-xl font-bold rounded-sm`}
+            >
               Technologies used:
-              <div className="gap-8 z-50 flex mt-4 flex-row justify-center items-center w-full">
-                <SiNestjs className="hover:text-neutral-400 transition-colors duration-500" size={32} />
-                <SiAngularjs className="hover:text-neutral-400 transition-colors duration-500" size={32} />
-                <SiTypescript className="hover:text-neutral-400 transition-colors duration-500" size={32} />
-                <SiCypress className="hover:text-neutral-400 transition-colors duration-500" size={32} />
-                <SiTailwindcss className="hover:text-neutral-400 transition-colors duration-500" size={32} />
-                <FaStripe className="hover:text-neutral-400 transition-colors duration-500" size={32} />
-              </div>
+              {TechIcons(techIcons)}
               <div className="gap-4 hover:text-blue-300 transition-colors duration-500 z-50 mt-4 flex flex-row justify-center items-center w-full">
-                <Link href={projectLink}>
+                <Link target={'_blank'} href={projectLink}>
                   <span className="text-xl uppercase">View Project</span>
                   <BsArrowUpRight className="inline ml-2" size={28} />
                 </Link>
@@ -68,6 +70,16 @@ export const ProjectItem = ({
           </div>
         </div>
       </div>
+    </div>
+  );
+};
+
+const TechIcons = (techIcons: Array<IconType>) => {
+  return (
+    <div className="gap-8 z-50 flex mt-4 flex-row justify-center items-center w-full">
+      {techIcons.map((Icon) => {
+        return <Icon className="hover:text-neutral-400 transition-colors duration-500" size={32} />;
+      })}
     </div>
   );
 };
--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -2,6 +2,17 @@ import { useEffect } from 'react';
 
 import { projectAnimations } from './projects-animation';
 import { ProjectItem } from './project-item';
+import {
+  SiAngularjs,
+  SiCypress,
+  SiNestjs,
+  SiPhp,
+  SiPhpmyadmin,
+  SiTailwindcss,
+  SiTypescript,
+  SiUnity,
+} from 'react-icons/si';
+import { FaStripe } from 'react-icons/fa';
 
 export default function Projects() {
   useEffect(() => {
@@ -22,24 +33,43 @@ export default function Projects() {
         id="projects-section"
         className="mt-4 min-h-screen projects basic-animation-values translate-x-[-210px] !duration-[1.5s]"
       >
-        <ProjectItem
-          title="Foodeli"
-          descriptionHeader="Build your own restaurant order website and receive new orders online."
-          descriptionInfo="Manage your orders in a few simple steps. With a single press on the button you can accept or cancel an
+        <div className="mt-4">
+          <ProjectItem
+            title="Foodeli"
+            descriptionHeader="Build your own restaurant order website and receive new orders online."
+            descriptionInfo="Manage your orders in a few simple steps. With a single press on the button you can accept or cancel an
                 order. Get an overview of all orders and related information."
-          projectLink="https://foodeli.nl/home"
-          imagePath="/backgrounds/foodeli.svg"
-          key={'https://foodeli.nl/home'}
-        />
-        <ProjectItem
-          title="POPs"
-          descriptionHeader="PeopleOverPapers is a website that helps job seekers find job opportunities, by displaying a wide range of job listings,"
-          descriptionInfo="including company reviews and salaries. Employers can also post new job listings, for free or as sponsored posts, making it a one-stop solution for both job seekers and employers."
-          projectLink="https://demo.overpaper.com/"
-          key={'https://demo.overpaper.com/'}
-          reverseOrder={true}
-          imagePath="/backgrounds/pops.svg"
-        />
+            projectLink="https://foodeli.nl/home"
+            imagePath="/backgrounds/foodeli.svg"
+            key={'https://foodeli.nl/home'}
+            techIcons={[SiNestjs, SiAngularjs, SiTypescript, SiCypress, SiTailwindcss, FaStripe]}
+          />
+        </div>
+
+        <div className="mt-4">
+          <ProjectItem
+            title="POPs"
+            descriptionHeader="PeopleOverPapers is a website that helps job seekers find job opportunities, by displaying a wide range of job listings,"
+            descriptionInfo="including company reviews and salaries. Employers can also post new job listings, for free or as sponsored posts, making it a one-stop solution for both job seekers and employers."
+            projectLink="https://demo.overpaper.com/"
+            key={'https://demo.overpaper.com/'}
+            reverseOrder={true}
+            imagePath="/backgrounds/pops.svg"
+            techIcons={[SiNestjs, SiAngularjs, SiTypescript, SiCypress, SiTailwindcss]}
+          />
+        </div>
+
+        <div className="mt-4">
+          <ProjectItem
+            title="Attack On Words"
+            descriptionHeader="Blast away invading ships in this thrilling typing adventure! Type specific phrases to shoot and defend against infinite levels of enemy ships. Perfect for those who love fast-paced action and sharpening their typing skills."
+            descriptionInfo="Get ready to be fully immersed in the exciting world of space battles!"
+            projectLink="https://github.com/ramishenouda/attackonwords"
+            key={'https://github.com/ramishenouda/attackonwords'}
+            imagePath="/backgrounds/attackonwords.svg"
+            techIcons={[SiUnity, SiPhp, SiPhpmyadmin]}
+          />
+        </div>
       </div>
     </section>
   );
